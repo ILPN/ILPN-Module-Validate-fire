a65f0d6ceba8cf27c4a87bee2e6b9daa9eb10fea
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-06-15 10:02:29 +0200

    copied common content from "validate a run" algorithm

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,14 +1,75 @@
 import {Component} from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
+import {
+    AlgorithmResult,
+    DropFile, FD_PARTIAL_ORDER, FD_PETRI_NET, PartialOrder,
+    PartialOrderParserService, PetriNet, PetriNetParserService
+} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     providers: [
-        // TODO base href
-        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/'}
+        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/dragon/'}
     ]
 })
 export class AppComponent {
+
+    private static readonly ALGORITHM_NAME = 'fire a run';
+    private static readonly RESULT_FILE_NAME = 'result.txt';
+
+    FD_PN = FD_PETRI_NET;
+    FD_PO = FD_PARTIAL_ORDER;
+
+    petriNet: PetriNet | undefined;
+    partialOrder: PartialOrder | undefined;
+
+    resultFile: DropFile | undefined;
+
+    constructor(private _pnParser: PetriNetParserService, private _poParser: PartialOrderParserService) {
+    }
+
+    parsePN(files: Array<DropFile>) {
+        if (files.length === 0) {
+            this.petriNet = undefined;
+        }
+        this.petriNet = this._pnParser.parse(files[0].content);
+        console.dir(this.petriNet);
+        this.validate();
+    }
+
+    parsePO(files: Array<DropFile>) {
+        if (files.length === 0) {
+            this.partialOrder = undefined;
+        }
+        this.partialOrder = this._poParser.parse(files[0].content);
+        console.dir(this.partialOrder);
+        this.validate();
+    }
+
+    private validate() {
+        if (this.petriNet !== undefined && this.partialOrder !== undefined) {
+            this.resultFile = undefined;
+            try {
+                // const validator = new LpoFlowValidator(this.petriNet, this.partialOrder);
+                //
+                // const start = performance.now();
+                // const results = validator.validate();
+                // const end = performance.now();
+                //
+                // const result = new AlgorithmResult(AppComponent.ALGORITHM_NAME, start, end);
+                // const places = this.petriNet.getPlaces();
+                // for (let i = 0; i < places.length; i++) {
+                //     result.addOutputLine(`${places[i].id} ${results[i] ? 'valid' : 'not valid'}`);
+                // }
+                // this.resultFile = result.toDropFile(AppComponent.RESULT_FILE_NAME);
+            } catch (e) {
+                const error = e as Error;
+                const result = new AlgorithmResult(AppComponent.ALGORITHM_NAME);
+                result.addOutputLine(error.message);
+                this.resultFile = result.toDropFile(AppComponent.RESULT_FILE_NAME);
+            }
+        }
+    }
 }
